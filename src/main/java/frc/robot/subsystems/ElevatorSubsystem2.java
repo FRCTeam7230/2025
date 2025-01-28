3116c7af3f534df79ce0d69054dd686e093f0262
ahanam7 <ahanamandalnandi@gmail.com>
2025-01-27 18:52:16 -0800

    new elevator code

---
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem2.java
@@ -0,0 +1,140 @@
+package frc.robot.subsystems;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+import com.revrobotics.RelativeEncoder;
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+import com.revrobotics.spark.SparkClosedLoopController;
+import com.revrobotics.spark.SparkMax;
+import com.revrobotics.spark.SparkBase.ControlType;
+import com.revrobotics.spark.SparkBase.PersistMode;
+import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.spark.config.SparkMaxConfig;
+import com.revrobotics.spark.config.SparkBaseConfig;
+import com.revrobotics.spark.ClosedLoopSlot;
+import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
+
+public class ElevatorSubsystem2 extends TimedRobot{
+  private SparkMax motor1;
+  private SparkMax motor2;
+  private SparkMaxConfig motorConfig1;
+  private SparkClosedLoopController closedLoopController;
+  private RelativeEncoder encoder;
+  private SparkBaseConfig motorConfig2;
+
+  public ElevatorSubsystem2() {
+    /*
+     * Initialize the SPARK MAX and get its encoder and closed loop controller
+     * objects for later use.
+     */
+    motor1 = new SparkMax(1, MotorType.kBrushless);
+    motor2 = new SparkMax(2, MotorType.kBrushless);
+    closedLoopController = motor1.getClosedLoopController();
+    encoder = motor1.getEncoder();
+    
+
+
+    /*
+     * Create a new SPARK MAX configuration object. This will store the
+     * configuration parameters for the SPARK MAX that we will set below.
+     */
+    motorConfig1 = new SparkMaxConfig();
+
+    /*
+     * Configure the encoder. For this specific example, we are using the
+     * integrated encoder of the NEO, and we don't need to configure it. If
+     * needed, we can adjust values like the position or velocity conversion
+     * factors.
+     */
+    motorConfig1.encoder
+        .positionConversionFactor(1)
+        .velocityConversionFactor(1);
+
+    /*
+     * Configure the closed loop controller. We want to make sure we set the
+     * feedback sensor as the primary encoder.
+     */
+    motorConfig1.closedLoop
+        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+        // Set PID values for position control. We don't need to pass a closed
+        // loop slot, as it will default to slot 0.
+        .p(0.4)
+        .i(0)
+        .d(0)
+        .outputRange(-1, 1)
+        // Set PID values for velocity control in slot 1
+        .p(0.0001, ClosedLoopSlot.kSlot1)
+        .i(0, ClosedLoopSlot.kSlot1)
+        .d(0, ClosedLoopSlot.kSlot1)
+        .velocityFF(1.0 / 5767, ClosedLoopSlot.kSlot1)
+        .outputRange(-1, 1, ClosedLoopSlot.kSlot1);
+
+    motorConfig1.closedLoop.maxMotion
+        // Set MAXMotion parameters for position control. We don't need to pass
+        // a closed loop slot, as it will default to slot 0.
+        .maxVelocity(1000)
+        .maxAcceleration(1000)
+        .allowedClosedLoopError(1)
+        // Set MAXMotion parameters for velocity control in slot 1
+        .maxAcceleration(500, ClosedLoopSlot.kSlot1)
+        .maxVelocity(6000, ClosedLoopSlot.kSlot1)
+        .allowedClosedLoopError(1, ClosedLoopSlot.kSlot1);
+
+    /*
+     * Apply the configuration to the SPARK MAX.
+     *
+     * kResetSafeParameters is used to get the SPARK MAX to a known state. This
+     * is useful in case the SPARK MAX is replaced.
+     *
+     * kPersistParameters is used to ensure the configuration is not lost when
+     * the SPARK MAX loses power. This is useful for power cycles that may occur
+     * mid-operation.
+     */
+    motor1.configure(motorConfig1, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    motorConfig2.follow(1,true);
+    motor2.configure(motorConfig2, ResetMode.kResetSafeParameters, PersistMode.kNoPersistParameters);
+    
+
+    // Initialize dashboard values
+    SmartDashboard.setDefaultNumber("Target Position", 0);
+    SmartDashboard.setDefaultNumber("Target Velocity", 0);
+    SmartDashboard.setDefaultBoolean("Control Mode", false);
+    SmartDashboard.setDefaultBoolean("Reset Encoder", false);
+  }
+
+  @Override
+  public void teleopPeriodic() {
+    if (SmartDashboard.getBoolean("Control Mode", false)) {
+      /*
+       * Get the target velocity from SmartDashboard and set it as the setpoint
+       * for the closed loop controller with MAXMotionVelocityControl as the
+       * control type.
+       */
+      double targetVelocity = SmartDashboard.getNumber("Target Velocity", 0);
+      closedLoopController.setReference(targetVelocity, ControlType.kMAXMotionVelocityControl,
+          ClosedLoopSlot.kSlot1);
+    } else {
+      /*
+       * Get the target position from SmartDashboard and set it as the setpoint
+       * for the closed loop controller with MAXMotionPositionControl as the
+       * control type.
+       */
+      double targetPosition = SmartDashboard.getNumber("Target Position", 0);
+      closedLoopController.setReference(targetPosition, ControlType.kMAXMotionPositionControl,
+          ClosedLoopSlot.kSlot0);
+    }
+  }
+
+  @Override
+  public void robotPeriodic() {
+    // Display encoder position and velocity
+    SmartDashboard.putNumber("Actual Position", encoder.getPosition());
+    SmartDashboard.putNumber("Actual Velocity", encoder.getVelocity());
+
+    if (SmartDashboard.getBoolean("Reset Encoder", false)) {
+      SmartDashboard.putBoolean("Reset Encoder", false);
+      // Reset the encoder position to 0
+      encoder.setPosition(0);
+    }
+  }
+}
