76ca50e34ec9318bb14c1459da1e4f60eb7bab83
Alex Danielsen <adh.danielsen@gmail.com>
2025-02-21 21:49:37 -0800

    Revert "Starting to Add SmartDashboard Interface for Paths"
    This reverts commit 2c2438c5cc4a3e9bc5cf19a5663ee461ac4ac4e3.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -198,7 +198,7 @@ public final class Constants {
     public static final double kMinElevatorHeightMeters = 0.0;
     //public static final double kMaxElevatorHeightMeters = 10.25;
     public static final double kMaxElevatorHeightMeters = 1.27;
-    public static final double kScoreElevatorHeightMeters = 1.17;//What is the height for this?
+
     public static final double kRotationToMeters = kElevatorDrumRadius * 2 * Math.PI;
     public static final double kRPMtoMPS = (kElevatorDrumRadius * 2 * Math.PI) / 60;
     public static final double kElevatorMaxVelocity = 3.5;
